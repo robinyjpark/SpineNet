5ab948707c18630c2363b9f3cfe9d51a9ec948e5
Robin Park <72387969+robinyjpark@users.noreply.github.com>
2024-09-17 17:19:40 +0100

    Change np.bool to bool

--- spinenet/utils/gen_utils.py
+++ spinenet/utils/gen_utils.py
@@ -367,7 +367,7 @@ def poly2mask(vertex_row_coords, vertex_col_coords, shape):
     fill_row_coords, fill_col_coords = draw.polygon(
         vertex_row_coords, vertex_col_coords, shape
     )
-    mask = np.zeros(shape, dtype=np.bool)
+    mask = np.zeros(shape, dtype=bool)
     mask[fill_row_coords, fill_col_coords] = True
     return mask
 
